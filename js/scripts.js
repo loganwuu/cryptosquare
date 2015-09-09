7cc3325ba6f86a54242f5291e6c3621db02dd12b
Logan Wu <student@epicodus.com>
2015-09-08 17:24:51 -0700

    finish scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,21 +22,11 @@ var formatString = function(str) {
     return array;
 };
 
-//get square/retangle form
-    var getArray = function(str) {
-        var array = str.split("");
-        return array;
-    };
-
-    var getLength = function(str) {
-        var strLength = removeSpace(str).length;
-        return strLength;
-    };
-
+//get square/retangle form to ready to encrypt
     var getSquare = function(str) {
         var strLength = removeSpace(str).length;
         var square = Math.sqrt(strLength);
-        colNum = Math.ceil(square);
+        var colNum = Math.ceil(square);
         return colNum;
     };
 
@@ -48,7 +38,42 @@ var formatString = function(str) {
         return array;
     };
 
+//final function
+var cryptoSquare = function(str) {
+    var array = formatString(str);
+    var square = Math.sqrt(array.length);
+    var colNum = Math.ceil(square);
+    var stringArray = array.chunk(colNum);
+    var characterArray = [];
+    for (entry in stringArray) {
+        var chars = stringArray[entry].split("");
+        characterArray.push(chars);
+    }
 
-var encrypt = function(str) {
+    var encryptedArray = [];
+    for(var j = 0; j < colNum; j += 1) {
+        for(var i = 0; i < colNum; i += 1) {
+            encryptedArray.push(characterArray[i][j]);
+        }
+    }
 
+    return encryptedArray;
 };
+
+$(document).ready(function() {
+    $("form#string").submit(function(event) {
+        $(".cryptoSquare").empty();
+        var string = $("input#string").val();
+        var result = cryptoSquare(string);
+        for (i in result) {
+            $(".cryptoSquare").append(result[i]);
+            if (((parseInt(i) + 1) % 20) === 0) {
+                $(".cryptoSquare").append("<br>");
+            } else if (((parseInt(i) + 1) % 5) === 0) {
+                $(".cryptoSquare").append(" ");
+            }
+        }
+        $("#result").show();
+        event.preventDefault();
+    });
+});
